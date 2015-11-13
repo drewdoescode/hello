4037e86851c234daf9c0551a0a35dfbb77e82c85
Drew Gibbs <drewdoesgamemaker@gmail.com>
2015-11-13 18:53:00 -0500

    save

--- java/src/main/java/hello.java
+++ java/src/main/java/hello.java
@@ -1,26 +1,92 @@
 /**
- * Not yo grandma's hello world is a less than simple Hello World
- * program that says the fundamentals of most all the command
- * line programs.
+ * Not yo grandma's hello world is not your regular, simple Hello World.
+ *
+ * This program that illustrates the fundamentals of most all command line
+ * programs including use of command line arguments and basic option
+ * switches (-m, -c).
  */
+
 import skilstak.c;
+
+/**
+ *  A class to print hello world-ish-ness to the terminal in different
+ *  ways.
+ */
 public class hello {
-    
-  public static void color(String message) {
-        System.out.println(c.rc() + message + " ");
-        }  
+
+    /**
+     * Prints inifinite randomly colored messages filling the terminal.
+     *
+     * @param message the message to print
+     */
+    public static void color(String message) {
+        System.out.println(c.clear);
+        while (true) {
+            System.out.print(c.rc() + message + " ");
+        }
     }
 
+    /**
+     * Prints a single message.
+     *
+     * @param message the message to print
+     */
     public static void plain(String message) {
         System.out.println(c.clear + message);
     }
-    public static void main(String[] args) {
-        String message = "world";
-        if (args.length > 0) {
+
+    /**
+     * Prints inifinite randomly colored messages Christmas style.
+     *
+     * @param message the message to print
+     */
+    public static void multi(String message) throws InterruptedException {
+        while (true) {
+            System.out.print(c.clear + c.multi(message));
+            Thread.sleep(500);
         }
-        //plain("Helloworld!");
-        color("Hello world!");
-   
+    }
+
+    public static String[] parseArgs(String[] args) {
+         String who = "world";
+         String option = "";
+         String message = "";
+         
+         if (args.length > 1) {
+             option = args[0];
+             who = args[1];
+         } else if (args.length == 1) {
+             if (args[0].startsWith("-")) {
+                 option = args[0];
+             } else {
+                 who = args[0];
+             }
+         }
+        String[] p = new String[2];
+        p[0] = who;
+        p[1] = option;
+
+        return p;
+
+    }   
+
+
+    public static void main(String[] args) throws InterruptedException {
+        String[] p = parseArgs(args);
+        String who = p[0];
+        String option = p[1];
+
+        String message = "Hello " + who + "!";
+        
+        // could do this more directly with reflection or an arg
+        // parser, but keeping is simple for now
+       
+        if (option.equals("-m")) {
+            multi(message);
+        } else if (option.equals("-c")) {
+            color(message);
+        } else {
+            plain(message);
+        }
     }
 }
-
---
+++ java/src/main/java/other.java
@@ -0,0 +1,7 @@
+public class other {
+
+    public static void main(String[] args) throws InterruptedException {
+        hello.printMulti("Hello world!");
+    }
+
+}
